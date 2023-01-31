8995cd989c0e5a84c4117aeabe0d5ebb1c8e565d
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-01-31 14:43:11 -0300

    criando a sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import styled from "styled-components";
 
-const PrincipalContainer = styled.main`
+import SideBar from "./components/SideBar";
+
+const BackgroundContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #85bcd530;
   display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PrincipalContainer = styled.main`
+  background-color: white;
+  padding: 15px;
+  height: 80%;
+  width: 70%;
+
+  display: flex;
+  border-radius: 10px;
+  gap: 4rem;
 `;
 
 function App() {
   return (
-    <PrincipalContainer>
-      <div>A pasta do projeto esta com as contas do vo</div>
-    </PrincipalContainer>
+    <BackgroundContainer>
+      <PrincipalContainer>
+        <SideBar />
+        <section>Content</section>
+      </PrincipalContainer>
+    </BackgroundContainer>
   );
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+import BgsideBar from "../assets/bg-sidebar-desktop.svg";
+
+const SideBarStyles = styled.section`
+  background-image: url(${BgsideBar});
+  background-size: cover;
+  background-position: bottom center;
+  padding: 0 4.1rem 0 1.5rem;
+  border-radius: 8px;
+
+  .containerInfoSideBarSteps {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.3rem;
+    color: white;
+  }
+
+  .sideBarInfoSteps {
+    display: flex;
+    gap: 1rem;
+    cursor: pointer;
+  }
+
+  .sideBarInfoSteps span {
+    border: 0.13rem solid white;
+    color: white;
+    width: 36px;
+    height: 36px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    font-weight: bold;
+    font-size: 0.9rem;
+  }
+  .sideBarInfoSteps h4 {
+    color: #a8a3a3;
+    font-weight: 400;
+  }
+  .sideBarInfoSteps h3 {
+    font-size: 1rem;
+  }
+`;
+
+export default function SideBar() {
+  return (
+    <SideBarStyles>
+      <div className="containerInfoSideBarSteps">
+        <div className="sideBarInfoSteps">
+          <span>1</span>
+          <div>
+            <h4>STEP 1</h4>
+            <h3>YOUR INFO</h3>
+          </div>
+        </div>
+        <div className="sideBarInfoSteps">
+          <span>2</span>
+          <div>
+            <h4>STEP 2</h4>
+            <h3>SELECT PLAN</h3>
+          </div>
+        </div>
+        <div className="sideBarInfoSteps">
+          <span>3</span>
+          <div>
+            <h4>STEP 3</h4>
+            <h3>ADD-ONS</h3>
+          </div>
+        </div>
+        <div className="sideBarInfoSteps">
+          <span>4</span>
+          <div>
+            <h4>STEP 4</h4>
+            <h3>SUMMARY</h3>
+          </div>
+        </div>
+      </div>
+    </SideBarStyles>
+  );
+}
